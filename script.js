5993f8a792ef182698d3b9d79ed0b1f5057e7370
Priyankasha01 <priyankasharrma388@gmail.com>
2024-08-20 02:14:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } 
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+        saveData();  // Save data after adding a task
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();  // Save data after toggling a task
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();  // Save data after removing a task
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+// Call showTask to display tasks on page load
+showTask();
+
